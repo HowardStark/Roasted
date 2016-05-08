7230f5058904a832b64c2a3a3e166d6d1be68076
HowardStark <howard@getcoffee.io>
2016-05-08 06:53:22 -0700

    Go server works

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "views/main.html")
 }
