a82a4519649e87d7c11cd9591f7c0b952cafb13c
HowardStark <howard@getcoffee.io>
2016-05-08 06:55:54 -0700

    Start-finish messages

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-
+	fmt.Println("Starting the roast...")
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	defer fmt.Println("The roast has finished...")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
